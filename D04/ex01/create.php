ee5868e1658f34634ef4c6d15d90e87b7b82ae80
Quentin BRAGARD <qbragard@e3r12p13.42.fr>
2019-10-03 18:08:24 +0200

    avant wk a Epinal 2

--- D04/ex01/create.php
+++ D04/ex01/create.php
@@ -5,5 +5,7 @@ if (!$_POST['login'] || !$_POST['passwd'])
     exit ;
 }
 if (!file_exists("../private/passwd"))
-    echo "OK\n";
+    mkdir("../private", 0777);
+file_put_contents("../private/passwd", $_POST['login'], FILE_APPEND);
+file_put_contents("../private/passwd", "\n", FILE_APPEND);
 ?>
