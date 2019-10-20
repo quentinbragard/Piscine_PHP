79d1c1ee11cba118fb0b50cbff4d2f6722509b74
Quentin Bragard <quentinbragard@MacBook-Pro-de-Quentin.local>
2019-10-20 19:48:30 +0200

    chart validation

--- Rush00/php/chart_validation.php
+++ Rush00/php/chart_validation.php
@@ -1,27 +1,37 @@
 <?php
 session_start();
-echo "ON";
-$price = $_SESSION['total'];
-echo "ON1";
+include("db_connect.php");
+$price = intval($_SESSION['Order_Price']);
 $chart = $_SESSION['chart'];
-echo "ON2";
-$length = count($chart) - 1;
-echo "ON3";
-$products = "";
-print_r($chart);
-echo "ON4";
-echo "login = ".$_SESSION['loggued_on_user'];
-echo "encore";
+$length = count($chart['Prod_Name']) - 1;
+$products = $chart['Prod_Name'][0];
+
 if (isset($_SESSION['loggued_on_user']))
 {
     $login = $_SESSION['loggued_on_user'];
-    while ($length >= 0)
+    while ($length > 0)
     {
-        if ($lenght != 0)
-            $products += $product."_";
+        $product = $chart['Prod_Name'][$length];
+        if ($length != 0)
+            $products = $products."_".$product;
         else    
-            $products += $product;
+            $products = $products.$product;
+        $length -= 1;
     }
-echo $products;
+    try
+    {
+        $conn = db_connect("localhost", "chez_maurice", "root", "123456");
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $sql = "INSERT INTO orders (ID, Email_Address, Products, Total_Price)
+        VALUES (NULL, '$login', '$products', '$price')";
+        $conn->exec($sql);
+    }
+    catch(PDOException $e)
+    {
+        echo "Error: " . $e->getMessage();
+        return (FALSE);
+    }
+    $conn = null;
+    echo "OK";
 }
 ?>
--- Rush00/php/test.php
+++
@@ -1,5 +0,0 @@
-<?php
-include("product_get.php");
-$tab = product_get();
-print_r($tab);
-?>
